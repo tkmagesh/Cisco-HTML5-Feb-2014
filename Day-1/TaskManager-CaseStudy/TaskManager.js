c3690d00155af2c5aa79253bbe2dafb63bb08ba6
tkmagesh <tkmagesh77@gmail.com>
2014-02-10 12:55:47 -0800

    Local Storage Implementation

--- Day-1/TaskManager-CaseStudy/TaskManager.js
+++ Day-1/TaskManager-CaseStudy/TaskManager.js
@@ -1,5 +1,11 @@
 (function(){
-	var txtTask, btnAdd, btnRemoveCompleted, divMessages, ulTaskList;
+	var txtTask
+		, btnAdd
+		, btnRemoveCompleted
+		, divMessages
+		, ulTaskList;
+		
+
 	window.addEventListener("DOMContentLoaded",init);
 	function init(){
 		txtTask = document.getElementById("txtTask");
@@ -10,12 +16,24 @@
 
 		btnAdd.addEventListener("click",onBtnAddClick);
 		btnRemoveCompleted.addEventListener("click",onBtnRemoveCompletedClick);
-
+		loadTasksFromStorage();
 	}
+	function loadTasksFromStorage(){
+		var tasks = getAllTasksFromStorage();
+		for(var i=0;i<tasks.length;i++)
+			addTaskToUi(tasks[i]);
+	}
+
 	function onBtnAddClick(){
 		var taskName = txtTask.value;
+		var newTask = addTaskToStorage(taskName);
+		addTaskToUi(newTask);
+	}
+
+	function addTaskToUi(newTask){
 		var newTaskItem = document.createElement("li");
-		newTaskItem.innerText = taskName;
+		newTaskItem.setAttribute("task-id",newTask.id);
+		newTaskItem.innerText = newTask.name;
 		newTaskItem.addEventListener("click",onTaskItemClick);
 		ulTaskList.appendChild(newTaskItem);
 	}
@@ -23,6 +41,8 @@
 		var taskItems = ulTaskList.children;
 		for(var i=taskItems.length-1;i>=0;i--){
 			if (taskItems[i].classList.contains("completed")){
+				var id = taskItems[i].getAttribute("task-id");
+				removeTaskFromStorage(id);
 				taskItems[i].remove();
 			}
 		}
